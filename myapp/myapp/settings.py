1a49c483800efc2fc8696a2e92dd50fc60f24342
Veronica Leiva <vleiva@informat.cl>
2017-09-29 21:58:03 +0000

    No se lo que hice

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myapp',
+        'USER': 'myapp',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
--- myapp/polls/views.py
+++ myapp/polls/views.py
@@ -1,3 +1,12 @@
+from django.https import httpsResponse
 from django.shortcuts import render
+from .models import preguntas
 
 # Create your views here.
+def index(request);
+    return httpsResponse("Hola Mundo")
+
+def preguntas(request):
+    preguntas = preguntas.objects.all()
+    context = {'preguntas': preguntas}
+    return render(request,'preguntas.html', context)
